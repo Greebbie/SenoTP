83c990ddca1c940b13a73884b8498fb99975d394
Midori <p65420201@gmail.com>
2025-04-11 02:08:42 -0400

    Initial Commit

---
+++ SenoTPTests/SenoTPTests.swift
@@ -0,0 +1,16 @@
+//
+//  SenoTPTests.swift
+//  SenoTPTests
+//
+//  Created by midori on 4/11/25.
+//
+
+import Testing
+
+struct SenoTPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
